1da49553546329845e06ed3bf682bff464ff53b0
Denys Harkusha <altamatick@icloud.com>
2025-06-26 14:05:08 +0200

    add message on line 9 in main

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Converter App.");
         double kgs = 5;
+        System.out.println("App for measures converting.");
         double pounds = convKgsToPounds(kgs);
         System.out.println("Result is" + pounds + " pounds.");
     }
