75239aa5c3637f649f574d5c64e04ccdd879bdd4
Denys Harkusha <altamatick@icloud.com>
2025-09-27 17:18:41 +0200

    homework 15

--- src/app/DataHandler.java
+++ src/app/DataHandler.java
@@ -1,18 +1,28 @@
 package app;
 
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class DataHandler {
 
-    private final ReentrantLock lock = new ReentrantLock();
+    Map<Integer, String> map = new DataRepository().getData();
 
-    public int modify(int num) {
-        lock.lock();
-        try {
-            num = num * 3;
-            return num;
-        } finally {
-            lock.unlock();
-        }
+    // Метод формує виведення нумерованого переліку імен
+    public String getAll() {
+        StringBuilder sb = new StringBuilder();
+        AtomicInteger count = new AtomicInteger(0);
+        map.forEach((id, name) ->
+                sb.append(String.format("%d) %d, %s%n",
+                        count.incrementAndGet(), id, name)
+        ));
+        return "\nALL NAMES:\n" + sb;
+    }
+
+    // Метод формує виведення імені за певним id
+    public String getById(int id) {
+        if (map.containsKey(id)) {
+            return "\nNAME: id " + id + ", " +
+                    map.get(id);
+        } else return "No data!";
     }
 }
--- src/app/DataRepository.java
+++ src/app/DataRepository.java
@@ -1,8 +1,16 @@
 package app;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DataRepository {
 
-    public int[] getData() {
-        return new int[] {7, 4, 5, 2};
+    public Map<Integer, String> getData() {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(387, "Lucy");
+        map.put(231, "Alice");
+        map.put(394, "Bob");
+        map.put(172, "Tom");
+        return map;
     }
 }
