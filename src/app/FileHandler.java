2cce1f670ba652909a3934d1f5f02e33e13e134e
Denys Harkusha <altamatick@icloud.com>
2025-09-27 16:35:33 +0200

    A functionality has been implemented that uses the File IO API capabilities to create a file and read it.

---
+++ src/app/FileHandler.java
@@ -0,0 +1,32 @@
+package app;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileHandler {
+
+    private final static String BASE_PATH = "files/";
+
+    public String writeFile(String fileName, String fileContent) {
+        try(FileWriter fw = new FileWriter(BASE_PATH + fileName)) {
+            fw.write(fileContent);
+            return "Success.";
+        } catch (Exception e) {
+            return "Error: " + e.getMessage();
+        }
+    }
+
+    public String readFile(String fileName) {
+        try (FileReader reader = new FileReader(BASE_PATH + fileName)) {
+            int sym;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((sym = reader.read()) != -1) {
+                stringBuilder.append((char) sym);
+            }
+            return stringBuilder.toString();
+        } catch (IOException ex) {
+            return ex.getMessage();
+        }
+    }
+}
--- src/app/Main.java
+++ src/app/Main.java
@@ -2,17 +2,16 @@ package app;
 
 public class Main {
 
+    private final static String BASE_PATH = "files/";
+
     public static void main(String[] args) {
-
-        DataProvider provider = new DataProvider();
-        DataHandler<String> stringHandler = new DataHandler<>();
-        DataHandler<Double> doubleHandler = new DataHandler<>();
-
-        String namesOutput = stringHandler.handleData(provider.getProductNames());
-        getOutput("Products: " + namesOutput);
-
-        String salesOutput = doubleHandler.handleData(provider.getSalesAmounts());
-        getOutput("Sales, EUR: " + salesOutput);
+        FileHandler handler = new FileHandler();
+        String fileName = "myfile.txt";
+        String fileContent = "My very important information.";
+        String result = handler.writeFile(fileName, fileContent);
+        String content = handler.readFile(fileName);
+        getOutput("RESULT: " + result);
+        getOutput("FILE CONTENT: " + content);
     }
 
     private static void getOutput(String output) {
